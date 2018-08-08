69b9e6735fc4b0c4e84a30dd744b19b2c846038f
wxjing4me <wu_xiao_jing@hotmail.com>
2018-08-08 18:15:21 +0800

    more hommization
    支持词组或者更多字数的短语啦~

--- README.md
+++ README.md
@@ -2,10 +2,30 @@
 
 > Chrome 扩展程序 ： 五笔编码查询
 
-界面就这样！
+界面就这样简单！
 
-![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page1.png)
+![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page0.png)
 
 输入汉字，回车，搞定！
 
+不论是一个字？
+
+![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page1.png)
+
+还是两个字的词组？
+
 ![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page2.png)
+
+亦或三个字的短语？
+
+![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page3.png)
+
+四个字的词语？
+
+![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page4.png)
+
+甚至更多字？
+
+![image](https://github.com/Kano-Wu/WubiQuery/blob/master/docs/page5.png)
+
+统统满足你！
--- js/process.js
+++ js/process.js
@@ -1,9 +1,12 @@
+let InputErrorMsg = '请输入中文汉字~'
+let RecommendMsg = '多个字的五笔编码仅供参考'
+let DefaultImgSrc = 'images/nopic.jpg'
 
 /* 隐藏五笔框 */
 function clearWubi() {
 	$('#show_hanzi').val('');
 	$('#show_wubi_code').val('');
-	$('#show_wubi_img').attr('src', 'images/nopic.jpg');
+	$('#show_wubi_imgs').html('');
 	$('#show_part').hide();
 	$('#show_tip').hide();
 }
@@ -14,13 +17,7 @@ function showWubi(hanzi) {
 	let wubi_img = 'gifs/'+hanzi+'.gif';
 	$('#show_hanzi').html(hanzi);
 	$('#show_wubi_code').html(wubi_code);
-	$('#show_wubi_img').attr('src', wubi_img);
-	
-	/* 图片不存在 */
-	$('#show_wubi_img').bind('error', function() {
-		$('#show_wubi_img').attr('src', 'images/nopic.jpg'); 
-	}); 
-
+	getWubiImg(hanzi);
 	$('#show_part').show();
 }
 
@@ -30,33 +27,87 @@ function showError(msg) {
 	$('#show_tip').show();
 }
 
-/* 获取汉字的五笔编码 TODO：支持词组*/
+/* 获取汉字的五笔编码 */
 function getWubi(hanzi) {
-	let wubi_code = Hanzi_Wubi[hanzi];
+	let wubi_code = '';
+	switch(hanzi.length) {
+		case 0: 
+			wubi_code = '';
+		case 1:
+			wubi_code = Hanzi_Wubi[hanzi];
+			break;
+		case 2: {
+			let wubi_code0 = Hanzi_Wubi[hanzi[0]];
+			let wubi_code1 = Hanzi_Wubi[hanzi[1]];
+			wubi_code = wubi_code0.substr(0, 2) + wubi_code1.substr(0, 2);
+			showError(RecommendMsg);
+			break;
+		}
+		case 3: {
+			let wubi_code0 = Hanzi_Wubi[hanzi[0]];
+			let wubi_code1 = Hanzi_Wubi[hanzi[1]];
+			let wubi_code2 = Hanzi_Wubi[hanzi[2]];
+			wubi_code = wubi_code0.substr(0, 1) + wubi_code1.substr(0, 1) + wubi_code2.substr(0, 2);
+			showError(RecommendMsg);
+			break;
+		}
+		default: {
+			let wubi_code0 = Hanzi_Wubi[hanzi[0]];
+			let wubi_code1 = Hanzi_Wubi[hanzi[1]];
+			let wubi_code2 = Hanzi_Wubi[hanzi[2]];
+			let wubi_codeX = Hanzi_Wubi[hanzi[hanzi.length-1]];
+			wubi_code = wubi_code0.substr(0, 1) + wubi_code1.substr(0, 1) + wubi_code2.substr(0, 1) + wubi_codeX.substr(0, 1);
+			showError(RecommendMsg);
+			break;
+		}
+	}
 	return wubi_code;
+}
+
+/* 获取汉字的拆分图解 */
+function getWubiImg(hanzi) {
+	hanzi = hanzi.split('');
+	if (hanzi.length < 4) {
+		// 0-3个字
+		for (var i = 0; i < hanzi.length; i++) {
+			let img = getWubiImgSrc(hanzi[i]);
+			$('#show_wubi_imgs').append(img);
+		}
+	} else {
+		// 4个字以上
+		for (var i = 0; i < 3; i++) {
+			let img = getWubiImgSrc(hanzi[i]);
+			$('#show_wubi_imgs').append(img);
+		}
+		let img = getWubiImgSrc(hanzi[hanzi.length-1]);
+		$('#show_wubi_imgs').append(img);
+	}
+}
+
+/* 汉字的拆分图-路径 */
+function getWubiImgSrc(hanzi) {
+	let src = "<img src='gifs/" + hanzi + ".gif' onerror='ifImgNotFound(this)'>";
+	return src;
 }
 
 /* 输入验证 
  * 除了汉字外的字都去掉
- * 只保留第一个汉字 TODO：支持词组
  */
 function checkInput(str) {
 	console.log('before: ' + str);
 	let reg = /[^\u4e00-\u9fa5]/g;
-	str = str.replace(reg, '').substr(0, 1);
+	str = str.replace(reg, '');
 	console.log('after : ' + str);
 	return str;
 }
 
 /* 测试 - 调整样式 */
 function testWubi() {
-	let hanzi = '搜';
-	let wubi_code = 'rvh';
-	let wubi_img = 'gifs/' + hanzi + '.gif';
+	let hanzi = '搜索';
+	let wubi_code = Hanzi_Wubi[hanzi];
+	let wubi_img = getWubiImg(hanzi);
 	$('#show_hanzi').html(hanzi);
 	$('#show_wubi_code').html(wubi_code);
-	$('#show_wubi_img').attr('src', wubi_img);
-	$('#show_part').show();
 }
 
 $(function(){
@@ -70,9 +121,9 @@ $(function(){
 	});
 
 	/* 文本框实时监测输入
-	 * 输入为空时，隐藏五笔显示框 
+	 * 输入为空时，重置五笔框 
 	 */
-	$("#input_hanzi").bind('input propertychange',function () {
+	$("#input_hanzi").bind('input propertychange', function () {
 		let hanzi = $('#input_hanzi').val();
 		if (hanzi == '') {
 			clearWubi();
@@ -84,17 +135,21 @@ $(function(){
 		e.preventDefault();
 		if (e.keyCode == 13) {
 			/* Enter键触发事件 */
+			clearWubi();
 			let hanzi = $('#input_hanzi').val();
 			let valid_hanzi = checkInput(hanzi);
 			if (valid_hanzi != '') {
 				showWubi(valid_hanzi);
 			} else {
-				showError('输入不合法');
+				showError(InputErrorMsg);
 			}
-	    } else if (e.keyCode == 9 ) {
-	    	/* Tab键触发事件   TODO：Ctrl+Enter */
-	    	console.log('Clear');
-	    }
+	    } 
 	});
 
+	/* TODO：清除快捷键（Ctrl+Enter）触发事件 */
 });
+
+/* 图片不存在 */
+function ifImgNotFound(that) {
+	$(that).attr('src', 'images/nopic.jpg'); 
+}
