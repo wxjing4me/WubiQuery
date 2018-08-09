bba264fb59c3f7f98f421afe7c88c2b06ab07103
wxjing4me <wu_xiao_jing@hotmail.com>
2018-08-09 11:43:42 +0800

    get more gifs

--- crawler/wubi_crawler.py
+++ crawler/wubi_crawler.py
@@ -1,56 +1,111 @@
+#-*-coding:utf-8-*-
+
 import requests
 from bs4 import BeautifulSoup
+import os
 
-base_url = 'http://www.52wubi.com'
-gifs_dir = '../gifs/'
+#---------------------------
+# 从52wubi上爬取数据
+#---------------------------
+def wubi_crawler_52wubi():
 
-wf = open('hanzi_wubi.txt', 'w')
+    base_url = 'http://www.52wubi.com'
+    gifs_dir = '../gifs/'
 
-for i in range(1, 7832):
+    wf = open('hanzi_wubi.txt', 'w')
 
-    print '>> ', i, '--------------------------'
+    for i in range(1, 7832):
 
-    inc = '/wbbmcx/wubi_' + str(i) + '.html'
-    url = base_url + inc 
-    response = requests.get(url)
-    response.encoding = 'gbk'
-    html_doc = response.text
+        print '>> ', i, '--------------------------'
 
-    soup = BeautifulSoup(
-        html_doc, 
-        'html.parser'
-    )
+        inc = '/wbbmcx/wubi_' + str(i) + '.html'
+        url = base_url + inc 
+        response = requests.get(url)
+        response.encoding = 'gbk'
+        html_doc = response.text
 
-    wubileft = soup.find('div', class_='wubileft')
-    table = wubileft.find('table')
-    trs = table.find_all('tr')
+        soup = BeautifulSoup(
+            html_doc, 
+            'html.parser'
+        )
 
-    hanzi = trs[1].find('td').get_text().strip()
-    pinyin = trs[2].find('td').get_text().strip()
-    wubi_code = trs[3].find('td').get_text().strip()
-    wubi_img_src = base_url + trs[4].find('img').get('src').strip()
+        wubileft = soup.find('div', class_='wubileft')
+        table = wubileft.find('table')
+        trs = table.find_all('tr')
 
-    print hanzi
-    print wubi_code
-    print wubi_img_src
+        hanzi = trs[1].find('td').get_text().strip()
+        pinyin = trs[2].find('td').get_text().strip()
+        wubi_code = trs[3].find('td').get_text().strip()
+        wubi_img_src = base_url + trs[4].find('img').get('src').strip()
 
-    
-    if len(hanzi) < 1:
-        continue
+        print hanzi
+        print wubi_code
+        print wubi_img_src
 
-    ## save hanzi and wubi
-    wf.write('"' + hanzi + '":"' + wubi_code + '"\n')
-    
-    ## get image
-    wubi_img = requests.get(wubi_img_src).content
+        
+        if len(hanzi) < 1:
+            continue
 
-    if wubi_img[:6] == '<html>':
-        pass
-    else:
-        file = gifs_dir + hanzi + '.gif'
-        with open(file, 'wb') as f:
-            f.write(wubi_img)
+        ## save hanzi and wubi
+        wf.write('"' + hanzi + '":"' + wubi_code + '"\n')
+        
+        ## get image
+        wubi_img = requests.get(wubi_img_src).content
 
-    
+        if wubi_img[:6] == '<html>':
+            pass
+        else:
+            file = gifs_dir + hanzi + '.gif'
+            with open(file, 'wb') as f:
+                f.write(wubi_img)
+
+        
+#---------------------------
+# 从cidianwang上爬取图片
+# 若52wubi已有图片，则pass，否则保存至gifs
+#---------------------------
+def wubi_crawler_cidianwang():
+    base_url = 'http://www.cidianwang.com/file/wubi/'
+    gifs_dir = '../gifs/'
+
+    hanziFilePath = 'hanzi_wubi.txt'
+
+    def isExistImg(hanzi):
+        img_file = gifs_dir + hanzi + '.gif'
+        return os.path.exists(img_file.decode('utf-8'))
+
+    def readHanziFile(file_path):
+        hanziList = []
+        with open(file_path, 'r') as f:
+            for line in f.readlines():
+                hanzi = line.strip().split('"')[1]
+                if not isExistImg(hanzi):
+                    hanziList.append(hanzi)
+        return hanziList
+
+    hanziList = readHanziFile(hanziFilePath)
+    for hanzi in hanziList:
+        ## get image
+        wubi_img_src = base_url + hanzi + '.gif'
+        # print hanzi
+        wubi_img = requests.get(wubi_img_src).content
+        
+        try:
+            if wubi_img[-1] == '>':
+                print wubi_img_src, 'pass'
+                pass
+            else:
+                print wubi_img_src, 'oik'
+                file = gifs_dir + hanzi + '.gif'
+                with open(file.decode('utf-8'), 'wb') as f:
+                    f.write(wubi_img)
+        except:
+            continue
+
+
+if __name__ == '__main__':
+    wubi_crawler_cidianwang()
+
+
 
 
